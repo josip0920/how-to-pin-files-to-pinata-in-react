e1eea409a62d609338b0966ad35e51ad51bec268
Josip0920 <111125255+josip0920@users.noreply.github.com>
2022-09-22 14:32:23 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,71 +1,62 @@
-import { useState } from 'react';
-import FormData from 'form-data';
-import axios from 'axios';
-
+import { useState } from "react";
+import FormData from "form-data";
+import axios from "axios";
 
 function App() {
+  const [file, setFile] = useState();
+  const [myipfsHash, setIPFSHASH] = useState("");
 
-  const [file, setFile] = useState()
-  const [myipfsHash, setIPFSHASH] = useState('')
- 
-
-  const handleFile=async (fileToHandle) =>{
-
-    
-
-    console.log('starting')
+  const handleFile = async (fileToHandle) => {
+    console.log("starting");
 
     // initialize the form data
-    const formData = new FormData()
-
-    // append the file form data to 
-    formData.append("file", fileToHandle)
+    const formData = new FormData();
+    const data = {
+      price: 12,
+      name: "raggadat",
+    };
+    // append the file form data to
+    formData.append("file", fileToHandle);
 
     // call the keys from .env
 
-    const API_KEY = process.env.REACT_APP_API_KEY
-    const API_SECRET = process.env.REACT_APP_API_SECRET
+    const API_KEY = process.env.API_KEY;
+    const API_SECRET = process.env.API_SECRET;
 
     // the endpoint needed to upload the file
-    const url =  `https://api.pinata.cloud/pinning/pinFileToIPFS`
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+    const jsonUrl = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
 
-    const response = await axios.post(
-      url,
-      formData,
-      {
-          maxContentLength: "Infinity",
-          headers: {
-              "Content-Type": `multipart/form-data;boundary=${formData._boundary}`, 
-              'pinata_api_key': API_KEY,
-              'pinata_secret_api_key': API_SECRET
+    const response = await axios.post(url, formData, {
+      maxContentLength: "Infinity",
+      headers: {
+        "Content-Type": `multipart/form-data;boundary=${formData._boundary}`,
+        pinata_api_key: API_KEY,
+        pinata_secret_api_key: API_SECRET,
+      },
+    });
 
-          }
-      }
-  )
+    console.log(response);
 
-  console.log(response)
-
-  // get the hash
-  setIPFSHASH(response.data.IpfsHash)
-
-  
-  }
-
-  
+    // get the hash
+    setIPFSHASH(response.data.IpfsHash);
+  };
 
   return (
     <div className="App">
-      <input type="file" onChange={(event)=>setFile(event.target.files[0])}/>
-      <button onClick={()=>handleFile(file)}>Pin</button>
-      
-      
-    {
+      <input type="file" onChange={(event) => setFile(event.target.files[0])} />
+      <button onClick={() => handleFile(file)}>Pin</button>
 
-      //  render the hash
-      myipfsHash.length > 0 && <img height='200' src={`https://gateway.pinata.cloud/ipfs/${myipfsHash}`} alt='not loading'/>
-    }
-    
-
+      {
+        //  render the hash
+        myipfsHash.length > 0 && (
+          <img
+            height="200"
+            src={`https://gateway.pinata.cloud/ipfs/${myipfsHash}`}
+            alt="not loading"
+          />
+        )
+      }
     </div>
   );
 }
